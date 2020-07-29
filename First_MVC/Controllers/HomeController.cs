30c3a8706b0143fecb6afa3e8d7fbde17114fbb2
jitendra0625@gmail.com <jitendra0625@gmail.com>
2020-07-29 23:48:52 +0100

    Test
    First Commit

---
+++ First_MVC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using First_MVC.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace First_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IConfiguration configuraton;
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public HomeController(IConfiguration config)
+        {
+            configuraton = config;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<WeatherForecast> reservationList = new List<WeatherForecast>();
+            using (var httpClient = new HttpClient())
+            {
+                string apiUrl =configuraton.GetValue<string>("APIURL");
+                using (var response = await httpClient.GetAsync(apiUrl))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<WeatherForecast>>(apiResponse);
+                }
+            }
+            ViewBag.Message = reservationList;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
