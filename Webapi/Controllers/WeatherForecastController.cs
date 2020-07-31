663466e283b7b76ffff2fc6c8726cf52533ca53f
Jitendra025 <jmg0625@gmail.com>
2020-07-31 01:22:50 +0100

    (feat): Added the DB connection

--- Webapi/Controllers/WeatherForecastController.cs
+++ Webapi/Controllers/WeatherForecastController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Webapi.Controllers
 {
@@ -26,14 +28,28 @@ namespace Webapi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            List<WeatherForecast> list = new List<WeatherForecast>();
+            //using (SqlConnection connection = new SqlConnection("Data Source=192.168.159.1,1433;Initial Catalog=dokcer_test;Integrated Security=true;")) Windowauthentication can' be use in docker on personal laptop
+                                                                                                                                                          //  as this gives error related to kerberose authentication that can be done uing Active directory and all
+            using (SqlConnection connection = new SqlConnection("Data Source=192.168.159.1,1433;Initial Catalog=dokcer_test;User ID=sa;Password=1234;"))
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                SqlCommand command = new SqlCommand("select * from weatherforcast", connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    WeatherForecast weatherForecast=  new WeatherForecast
+                    {
+                        Date = Convert.ToDateTime(reader[0]),
+                        TemperatureC = Convert.ToInt16(reader[1]),
+                        Summary = Convert.ToString(reader[2])
+                    };
+                    list.Add(weatherForecast);
+
+                }
+            }
+            var rng = new Random();
+            return list.ToArray();
         }
     }
 }
